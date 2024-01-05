130ac139d544f3680b2a1cf4b4ae1d624f38239c
ponpon <ponpon@flydove.net>
2024-01-06 03:33:25 +0800

    procfile

--- resources/views/machines.blade.php
+++ resources/views/machines.blade.php
@@ -56,7 +56,7 @@
             <td>{{ $machine['dnplayer_running'] }}/{{ $machine['dnplayer'] }}</td>
             <td>{{ $machine['mac'] }}</td>
             <td>{{ $machine['data']['last_updated'] }}</td>
-            <td>@foreach ($machine['m_info'] as $service => $value){{ $machine['m_info'] }} : {{ $value }}@endforeach</td>
+            <td>@foreach ($machine['m_info'] as $service => $value){{ $service }} : {{ $value }}@endforeach</td>
             <td>
                 <!-- 删除按钮 -->
                 <button class="delete-btn" data-token="{{ $token }}" data-mac="{{ $machine['mac'] }}">清除設置錯誤電腦</button>
