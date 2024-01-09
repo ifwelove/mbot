0a324e5232ac1039519a0510535d0e5181be446e
ponpon <ponpon@flydove.net>
2024-01-09 17:16:02 +0800

    procfile

--- config/monitor-token.php
+++ config/monitor-token.php
@@ -121,9 +121,4 @@ return [
         'date'   => '2025-01-01',
         'amount' => '10',
     ],
-    'OVQPsS1yS99vCbfAX9hRY2YOvZ72pPQXFnSbj5WIcfu'  => [
-        'name'   => '',
-        'date'   => '2025-01-01',
-        'amount' => '10',
-    ],
 ];
