e8795447e14276ae5c1b4c3108d6dc3a2aa7810f
ponpon <ponpon@flydove.net>
2024-01-24 00:55:15 +0800

    procfile

--- app/Http/Controllers/AlertController.php
+++ app/Http/Controllers/AlertController.php
@@ -451,7 +451,7 @@ class AlertController extends Controller
                         if (isset($m_info['card'])) {
                             $card = str_replace('?', '時', $m_info['card']);
                             dump($card);
-                            if (preg_match('/(\d+)天\s*(\d+)時/', $card, $matches)) {
+                            if (preg_match('/(\d+)天\s*(\d+)小時/', $card, $matches)) {
                                 dump($matches);
                                 // 使用當前時間，加上解析出來的天數和小時數
                                 $days           = $matches[1];
