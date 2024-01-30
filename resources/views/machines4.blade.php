50e274f012f4134660363008c29beffede63f4f3
ponpon <ponpon@flydove.net>
2024-01-30 15:53:56 +0800

    procfile

--- resources/views/machines4.blade.php
+++ resources/views/machines4.blade.php
@@ -49,7 +49,7 @@
     <p>綠燈 正常運作, 黃燈 大尾沒開, 紅燈 大尾沒回應, 灰色 主機沒訊號</p>
     <p>使用期限：{{ $user['date'] }}, 可使用台數：{{ $user['amount'] }}</p>
     <p>共有礦場 {{ $machines_total }} 座, 有打幣機正在挖礦中 {{ $dnplayer_running_total }} / {{ $dnplayer_total }}</p>
-    <p>全伺服器統計：{{ $money_total }}, 各伺服器鑽石統計：<select name="server" class="custom-select">
+    <p>全伺服器統計：{{ $money_total }}, 平均帳號打鑽數：{{ $money_total / $dnplayer_total }}, 各伺服器鑽石統計：<select name="server" class="custom-select">
             @foreach ($merges as $server => $total)
                 <optgroup label="{{ $server }}">
                     @php
