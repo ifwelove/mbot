4cfc9aacf9ced6f070a43a3482cfdc5decc384b7
ponpon <ponpon@flydove.net>
2024-03-09 00:29:30 +0800

    procfile

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\Http\Controllers\ProxyController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::get('/latest-filename', [ProxyController::class, 'getLatestFileName']);
