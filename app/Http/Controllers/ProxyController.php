3657dff52ff91ac3def85a20bf8bfbe3c81bb84a
ponpon <ponpon@flydove.net>
2024-10-16 20:32:01 +0800

    procfile

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -17,20 +17,20 @@ class ProxyController extends Controller
     public function getLatestFileName(Request $request)
     {
         // 从 Redis 缓存中获取最新文件名，如果不存在或已过期则执行回调
-//        $latestFileName = Cache::remember('latest_file_name', 300, function () {
-//            // 向文件服务器发送请求获取最新文件名
-//            $response = Http::get($this->latestFilename);
-//
-//            // 确认响应成功并获取文件名，否则返回默认值
-//            if ($response->successful() && $response->json('latestFileName')) {
-//                return $response->json('latestFileName');
-//            } else {
-//                // 根据需要处理错误或返回默认值
-//                return 'No file found';
-//            }
-//        });
+        $latestFileName = Cache::remember('latest_file_name', 10, function () {
+            // 向文件服务器发送请求获取最新文件名
+            $response = Http::get($this->latestFilename);
+
+            // 确认响应成功并获取文件名，否则返回默认值
+            if ($response->successful() && $response->json('latestFileName')) {
+                return $response->json('latestFileName');
+            } else {
+                // 根据需要处理错误或返回默认值
+                return 'No file found';
+            }
+        });
 //        $latestFileName = '大尾3-9.7.9-v1.7.19.rar';
-        $latestFileName = '大尾3-2.0.0.2-v1.7.25.rar';
+//        $latestFileName = '大尾3-2.0.0.2-v1.7.25.rar';
         return response()->json(['latestFileName' => $latestFileName]);
 //        return response($latestFileName, 200)->header('Content-Type', 'text/plain');
     }
