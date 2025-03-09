653374b44bbe30947eab411ed2aad66740b19dc6
ponpon <ponpon@flydove.net>
2025-03-09 18:24:29 +0800

    procfile

---
+++ config/dompdf.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'font_path' => storage_path('fonts/'),
+    'font_cache' => storage_path('fonts/'),
+    'default_font' => 'NotoSansTC'
+];
--- resources/views/quotation/pdf.blade.php
+++ resources/views/quotation/pdf.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <style>
-        body { font-family: 'DejaVu Sans', sans-serif; }
+        body { font-family: 'NotoSansTC', 'DejaVu Sans', sans-serif; }
         .header { text-align: center; }
         .logo { width: 150px; height: auto; }
         .table { width: 100%; border-collapse: collapse; margin-top: 20px; }
         .table th, .table td { border: 1px solid black; padding: 8px; text-align: left; }
     </style>
+
 </head>
 <body>
 <div class="header">
