09edaaa48c3befac8785ce7bf5bdb1966d995642
ponpon <ponpon@flydove.net>
2025-03-14 18:33:32 +0800

    procfile

--- resources/views/machines0314.blade.php
+++ resources/views/machines0314.blade.php
@@ -93,9 +93,9 @@
         <button class="btn btn-primary" data-toggle="modal" data-target="#dataModal">
             顯示資料
         </button>
-        <button class="btn btn-warning btn-sm mb-2"
+        <button class="btn btn-warning mb-2"
                 onclick="copyToClipboard('#all-data')">
-            複製鑽石資訊
+            複製全服鑽石資訊
         </button><br>
         <div id="all-data" style="display:none;">@foreach ($merges as $key => $value){{ $key }}: {{ $value }}<br>@endforeach
         </div>
