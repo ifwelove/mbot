dc0aadba1b138ab6a7aa7fbffd3e1498a2300381
ponpon <ponpon@flydove.net>
2025-03-14 17:10:34 +0800

    procfile

--- resources/views/demo2.blade.php
+++ resources/views/demo2.blade.php
@@ -470,10 +470,10 @@
         </div>
     </div>
 @endforeach
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+{{--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>--}}
 <!-- jQuery + Bootstrap JS (4.x) -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+{{--<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>--}}
 <!-- 需要 AJAX POST 或其他功能時，可以換更高版本 (3.5.1) -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
